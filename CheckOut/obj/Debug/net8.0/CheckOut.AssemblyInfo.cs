d9364557cf4c76e556dbd314fb076bb8f18470c9
LiamJGahan <liamgahan09@gmail.com>
2025-04-18 17:43:03 +0100

    Updating Workflows for Azure due to wrong pathway

--- CheckOut/obj/Debug/net8.0/CheckOut.AssemblyInfo.cs
+++ CheckOut/obj/Debug/net8.0/CheckOut.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CheckOut")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+282bf0626b18b3264ae9be6fc83eb8566268cff0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+526e31359c566cf13c52559c16b71bb397b8f093")]
 [assembly: System.Reflection.AssemblyProductAttribute("CheckOut")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CheckOut")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
